064934b4a232658dc3ee77bcbdb60d3f8ee4c59c
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-03-25 00:16:49 +0900

    fixe changes swagger

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-const app = express();
 const cors = require('cors');
-
+const app = express();
 const port = process.env.PORT || 3001;
 
+//cors setup
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }));
+
+// Swagger UI setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
-app.use((req, res, next)=> {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT, DELETE, OPTIONS');
-    next();
-});
+// app.use((req, res, next)=> {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+//     );
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT, DELETE, OPTIONS');
+//     next();
+// });
 
-//cors setup
-app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }));
-app.use(cors({ origin: '*' }));
+
 
 //import and use routes
 app.use('/', require('./routes'));
